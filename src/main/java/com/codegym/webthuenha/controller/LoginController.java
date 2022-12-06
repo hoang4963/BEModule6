37f66237d004973cab7dc440cc072340cde1ba1f
quanph14 <phquan.0493@gmail.com>
2022-12-06 21:16:58 +0700

    done jwt

---
+++ src/main/java/com/codegym/webthuenha/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.codegym.webthuenha.controller;
+
+import com.codegym.webthuenha.model.JwtResponse;
+import com.codegym.webthuenha.model.User;
+import com.codegym.webthuenha.service.jwt.JwtService;
+import com.codegym.webthuenha.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+public class LoginController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private IUserService userService;
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user){
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String jwt = jwtService.createToken(authentication);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            User currentUser = userService.findByUsername(user.getUsername());
+            return ResponseEntity.ok(new JwtResponse(jwt, currentUser.getId(), userDetails.getUsername(), userDetails.getAuthorities()));
+        } catch (Exception e) {
+            return ResponseEntity.ok("Not Found User");
+        }
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> admin() {
+        return new ResponseEntity<>("Admin", HttpStatus.OK);
+    }
+    @GetMapping("/user")
+    public ResponseEntity<String> user() {
+        return new ResponseEntity<>("User", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/codegym/webthuenha/service/user/IUserService.java
@@ -0,0 +1,9 @@
+package com.codegym.webthuenha.service.user;
+
+import com.codegym.webthuenha.model.User;
+import com.codegym.webthuenha.service.IGeneralService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface IUserService extends IGeneralService<User>, UserDetailsService {
+    User findByUsername(String username);
+}
