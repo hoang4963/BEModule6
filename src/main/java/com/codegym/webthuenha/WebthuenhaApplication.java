03f45bf1ecbfc6809432f8b7fa1de3a7dc7887c2
hoang4963 <hoangth.finbox@gmail.com>
2022-12-06 08:42:29 +0700

    first commit

---
+++ src/main/java/com/codegym/webthuenha/WebthuenhaApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.webthuenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebthuenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebthuenhaApplication.class, args);
+	}
+
+}
