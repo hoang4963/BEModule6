3437b55e1dd7959945b3fe858bf3a59f939762a2
hoang4963 <hoangth.finbox@gmail.com>
2022-12-09 15:35:11 +0700

    avatar list

--- src/main/java/com/codegym/webthuenha/repository/IImageRepository.java
+++ src/main/java/com/codegym/webthuenha/repository/IImageRepository.java
@@ -13,6 +13,6 @@ public interface IImageRepository extends JpaRepository<Image, Long> {
     @Query(nativeQuery = true, value = "select  * from `images` where image_name = :name limit 1")
     Optional<Image> searchImageByImageName(@Param("name") String name);
 
-    @Query(nativeQuery = true, value = "select images.* from images join houses_image where house_id = :id group by images.id;")
-    Iterable<Image> searchImageByHouseId(@Param("id") Long id);
+    @Query(nativeQuery = true, value = "select images.* from images join houses_image on images.id = houses_image.image_id where house_id = :idabcd ")
+    Iterable<Image> searchImageByHouseId(@Param("idabcd") Long id);
 }
