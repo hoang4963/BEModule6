781809f9ac825e71e58c7e41ca8cf447381c611e
Duonghieu97st <Duonghieu97st@gmail.com>
2022-12-14 11:04:28 +0700

    update

--- src/main/java/com/codegym/webthuenha/repository/IOrderRepository.java
+++ src/main/java/com/codegym/webthuenha/repository/IOrderRepository.java
@@ -13,7 +13,7 @@ public interface IOrderRepository extends JpaRepository<Order, Long> {
     @Query(nativeQuery = true, value = "select * from orders where house_id = :id and ((orders.start_time <= :startTime and orders.end_time >= :startTime) or (orders.start_time <= :endTime and orders.end_time >= :endTime))")
     Iterable<Order> checkOrder(@Param("id") Long id, @Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
-    @Query(nativeQuery = true, value = "select * from orders where house_id = :id")
+    @Query(nativeQuery = true, value = "select * from orders where house_id = :id and (orders.order_status_id =2)")
     Iterable<Order> showOrderByHouseId(@Param("id") Long id);
 
     @Query(nativeQuery = true, value = "select o.* from orders o join houses h on h.id = o.house_id where h.user_id = :idabcd and o.order_status_id = 1")
